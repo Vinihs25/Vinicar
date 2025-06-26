6c0142aedb9bbd839362d68c249f92db8abbd3ff
Vinihs25 <vini.henriquedb@gmail.com>
2025-06-26 10:03:21 -0300

    menu responsivo

--- script.js
+++ script.js
@@ -63,7 +63,14 @@ revelar.reveal('.about', {
 });
 
 
+//menu hamburg
 
+const menuBtn = document.getElementById('menu-btn');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
 
 
 
